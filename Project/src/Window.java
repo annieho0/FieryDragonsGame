267bb88ba353e994cfc6c80e6bfe421becc0ab93
Annie <ahoo0034@student.monash.edu>
2024-04-29 10:56:49 +1000

    [add] called the GamePanel class to the Window class so that the window can open the game panel

--- Project/src/Window.java
+++ Project/src/Window.java
@@ -3,11 +3,17 @@ import java.awt.image.BufferStrategy;
 
 public class Window extends JFrame{
     private BufferStrategy bs;
+    private GamePanel gamePanel;
     public Window() {
         setTitle("Fiery Dragons");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setVisible(true);
+        gamePanel = new GamePanel(); // Initialize the GamePanel
+        getContentPane().add(gamePanel);
+        pack();
+
+        gamePanel.startGameThread();
     }
 
     public void addNotify() {
