94742871c4fa780b03c086749fd7a4d1ca97e1cd
Navya Balraj <nbal0016@student.monash.edu>
2024-05-05 19:51:59 +1000

    changed Cave and Gameboard so that gameboard adds each cave separately. started javadoc

--- src/Action.java
+++ src/Action.java
@@ -1,9 +1,12 @@
 
 /**
- * Abstract Base class for Actions. These represent events that happen in the game and things that dragons can do
+ * Abstract Base class for Actions. These represent events that happen in the game and things that dragons can do.
  */
 public abstract class Action {
 
+  /**
+   * Execute method which will execute the effects of the action. It returns a String stating the success or failure of the action.
+   */
   public abstract String execute();
 
 }
