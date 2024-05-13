1576ccbe17fd256f4a04447af440d315f4ce1e8b
Annie <ahoo0034@student.monash.edu>
2024-05-14 02:51:11 +1000

    [add] enum for player turns

--- fieryDragons/src/player/Player.java
+++ fieryDragons/src/player/Player.java
@@ -1,5 +1,6 @@
 package player;
 
+import dragonCards.Cards;
 import main.GamePanel;
 
 import javax.imageio.ImageIO;
@@ -10,9 +11,11 @@ import java.io.IOException;
 public abstract class Player extends Entity{
     protected GamePanel gp;
     protected BufferedImage dragonImage;
+    protected PlayerTurn playerTurn;
 
     public Player(GamePanel gp, String imagePath, int startX, int startY) {
         this.gp = gp;
+        this.playerTurn = playerTurn;
         setDefaultValues(startX, startY);
         getDragonImage(imagePath);
     }
@@ -30,6 +33,18 @@ public abstract class Player extends Entity{
             e.printStackTrace();
         }
     }
+    public PlayerTurn getPlayerTurn() {
+        return playerTurn;
+    }
+
+    public void setPlayerTurn(PlayerTurn playerTurn) {
+        this.playerTurn = playerTurn;
+    }
+
+    // Method to switch player turn
+    public void switchPlayerTurn() {
+        playerTurn = (playerTurn == PlayerTurn.PINK) ? PlayerTurn.PURPLE : PlayerTurn.PINK;
+    }
     public boolean contains(int mouseX, int mouseY) {
         return mouseX >= x && mouseX <= x + gp.tileSize &&
                 mouseY >= y && mouseY <= y + gp.tileSize;
@@ -44,6 +59,9 @@ public abstract class Player extends Entity{
         x = positionX * gp.tileSize;
         y = positionY * gp.tileSize;
     }
+    public abstract boolean hasMatches();
+    public abstract String getDragonCardImage();
+
 
     public int getX() {
         return x;
@@ -53,28 +71,4 @@ public abstract class Player extends Entity{
         return y;
     }
 }
-//    GamePanel gp;
-//    public Player(GamePanel gp){
-//        this.gp = gp;
-//        setDefaultValues(4,12);
-//        getPlayerImage();
-//
-//    }
-//    public void setDefaultValues(int startX, int startY){
-//        x = startX * gp.tileSize;
-//        y = startY * gp.tileSize;
-//    }
-//    public void getPlayerImage(){
-//        try{
-//            dragon = ImageIO.read(getClass().getResourceAsStream("/players/BlueDragon.png"));
-//        }
-//        catch (IOException e){
-//            e.printStackTrace();
-//        }
-//    }
-//    public void update(){}
-//    public void draw(Graphics2D g2){
-//        BufferedImage image = dragon;
-//        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
-//    }
-//}
+
